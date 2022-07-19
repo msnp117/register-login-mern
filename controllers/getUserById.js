ca43de2f30e2c5982cdda4fe3853c4bdc9f80c73
msnp117 <solemza530@gmail.com>
2022-07-18 21:19:28 -0300

    getUserById controller finished

--- controllers/getUserById.js
+++ controllers/getUserById.js
@@ -1,7 +1,20 @@
-const Usuario = require('../models/usuario')
+const Usuario = require("../models/usuario");
 
 const getUserById = async (req, res) => {
-    
-}
+  const { id } = req.params;
 
-module.exports = getUserById
+  if (id.length === 24) {
+    Usuario.findById(id).then((usuario) => {
+      if (!usuario) {
+        return res.json({ mensaje: "Usuario no encontrado" });
+      } else {
+        const { _id, contraseña, __v, ...resto } = usuario._doc;
+        res.json(resto);
+      }
+    });
+  } else {
+    res.json({ mensaje: "Estas enviando una contraseña incorrecta" });
+  }
+};
+
+module.exports = getUserById;
